93a91eb4fcb1af28c79b04a21933d5393242e089
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-15 23:10:48 -0600

    mvp

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,34 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db('accounts')
+};
 
 const getById = id => {
   // DO YOUR MAGIC
-}
+  return db('accounts').where("id", id).first();
+};
 
 const create = account => {
   // DO YOUR MAGIC
-}
+  const [id] = await db('accounts').insert({ account })
+  const newAccount = await getById(id)
+  return newAccount
+};
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
-}
+  return db('accounts').where('id', id).update({ id, account })
+    .then(() => {
+      return getById(id)
+    })
+};
 
 const deleteById = id => {
   // DO YOUR MAGIC
-}
+  return db('accounts').where('id', id).del()
+};
 
 module.exports = {
   getAll,
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,67 @@
 const router = require('express').Router()
+const Account = require('./accounts-model');
+const mw = require('./accounts-middleware');
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get('/:id', mw.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const accounts = await Account.getById(req.params.id);
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post('/', mw.checkAccountPayload, mw.checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const newAccount = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget,
+    });
+    res.status(201).json(newAccount);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', mw.checkAccountId, mw.checkAccountPayload, async (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    const update = await Account.updateById(req.params.id, req.body);
+    res.json(update);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', mw.checkAccountId, async (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// eslint-disable-next-line
+router.use((err, req, res, next) => { 
+  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+});
 
 module.exports = router;
